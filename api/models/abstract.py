323754d9d9d9c9cc316fcf9f7e852dfda9f6a062
smsamlee <smsamlee007@gmail.com>
2025-06-25 23:15:01 +0900

    abstract 모델 추가

---
+++ api/models/abstract.py
@@ -0,0 +1,21 @@
+from uuid import uuid4
+from datetime import datetime
+
+from sqlalchemy import String, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from api.utils.datadase import Base
+
+class AbstractBase(Base):
+    __abstract__ = True
+
+    id: Mapped[str] = mapped_column(
+        String, 
+        primary_key=True, 
+        index=True,
+        default=lambda: str(uuid4)
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now
+    )
